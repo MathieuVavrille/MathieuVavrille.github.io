5077079da652bc39dc9188f42eb4ece8d189969e
Mathieu Vavrille <mathieu.vavrille@waltr.fr>
2024-12-21 00:16:32 +0100

    working fine, I will fill it now

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -3,26 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     toggles.forEach((toggle) => {
         toggle.addEventListener("click", () => {
-	    console.log("test");
             const section = toggle.parentElement;
             const details = section.querySelector(".cv-details");
-            // Toggle the active state
             section.classList.toggle("active");
-	    console.log(details.scrollHeight);
 
             if (section.classList.contains("active")) {
-                // Collapse: Transition to max-height 0
                 details.style.maxHeight = details.scrollHeight + "px";
             } else {
-                // Expand: Dynamically calculate scrollHeight
-                details.style.maxHeight = "0";
+                details.style.maxHeight = null;
             }
-
         });
-
-        // Ensure proper collapsing on page load
-        const details = toggle.parentElement.querySelector(".cv-details");
-        details.style.maxHeight = "0";
-
     });
 });
